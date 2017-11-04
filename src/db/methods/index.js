e185d97a6b0190cf43d458b4768c7b172563682d
speckospock <17017460+speckospock@users.noreply.github.com>
2017-11-04 10:25:58 -0700

    Further improves src/db/methods/index.js readability

--- src/db/methods/index.js
+++ src/db/methods/index.js
@@ -1,37 +1,3 @@
-// module.exports = {
-//   generateFakeData: require('./generateFakeData'),
-//   topBuys: require('./topBuys'),
-//   topSells: require('./topSells'),
-//   openPosition: require('./openPosition'),
-//   updatePosition: require('./updatePosition'),
-//   resolvePosition: require('./resolvePosition'),
-//   closeOrder: require('./closeOrder'),
-//   processOrder: require('./processOrder'),
-//   clearDB: require('./clearDB'),
-// };
-
-// import generateFakeData from './generateFakeData';
-// import topBuys from './topBuys';
-// import topSells from './topSells';
-// import openPosition from './openPosition';
-// import updatePosition from './updatePosition';
-// import resolvePosition from './resolvePosition';
-// import closeOrder from './closeOrder';
-// import processOrder from './processOrder';
-// import clearDB from './clearDB';
-
-// export {
-//   generateFakeData,
-//   topBuys,
-//   topSells,
-//   openPosition,
-//   updatePosition,
-//   resolvePosition,
-//   closeOrder,
-//   processOrder,
-//   clearDB,
-// };
-
 export { generateFakeData } from './generateFakeData';
 export { topBuys } from './topBuys';
 export { topSells } from './topSells';
@@ -41,4 +7,3 @@ export { resolvePosition } from './resolvePosition';
 export { closeOrder } from './closeOrder';
 export { processOrder } from './processOrder';
 export { clearDB } from './clearDB';
-
